3689b249c57b6c7bbf4ee762a142bd9fbc0997ab
Giacomo <Giacomo@Giacomo-PC>
2019-09-24 11:31:31 +0200

    Pagina payment con tutte le modifiche, inserito nuovo campo nome /cognome separato nei due popup e creato un nuovo popup, nel caso salvando il preventivo l'anagrafica del cliente risulta già presente, attivabile eseguendo: $("#user-messages").modal();

--- js/script.js
+++ js/script.js
@@ -1428,6 +1428,10 @@
                 $(".agency-cral").hide();
             }
         });
+        //apro login da pagina pagamento
+        function apriLogin() {
+            $("#login-form-reg").show();
+        }
 
         /*===== Modal change password =====*/
         $(".reset-pass-button").click(function (e) {
